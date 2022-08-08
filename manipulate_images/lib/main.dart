b34f66634203a60a47de5f35312003c3bf2c5221
Felipe Assis <felipe.assis@MACBOOKSOFT-VIII.local>
2022-08-08 09:44:54 -0300

    Implements select source

--- manipulate_images/lib/main.dart
+++ manipulate_images/lib/main.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:manipulate_images/profile/profile_screen.dart';
+import 'package:manipulate_images/profile/controller/profile_controller.dart';
+import 'package:manipulate_images/profile/view/profile_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    Provider(create: (_) => ProfileController()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          appBarTheme: const AppBarTheme(
-            systemOverlayStyle: SystemUiOverlayStyle.light,
-          ),
-          primarySwatch: Colors.blue,
-          backgroundColor: Colors.white,
-          textTheme: GoogleFonts.nunitoSansTextTheme()),
-      home: const HomePage(),
-    );
+    return ScreenUtilInit(builder: (context) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle(
+                  statusBarBrightness: Brightness.light,
+                  systemNavigationBarIconBrightness: Brightness.light),
+            ),
+            primarySwatch: Colors.blue,
+            backgroundColor: Colors.white,
+            textTheme: GoogleFonts.nunitoSansTextTheme()),
+        home: const HomePage(),
+      );
+    });
   }
 }
--- manipulate_images/lib/shared/theme/custom_app_bar.dart
+++ manipulate_images/lib/shared/theme/custom_app_bar.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:manipulate_images/shared/theme/texts_component.dart';
+import 'package:manipulate_images/shared/theme/app_colors.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({Key? key}) : super(key: key);
@@ -11,109 +14,40 @@ class CustomAppBar extends StatelessWidget {
         ClipPath(
           clipper: WaveClipperTwo(),
           child: Container(
-            height: 250,
+            height: 250.h,
             decoration: const BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.center,
                 end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFF0061ff),
-                  Color(0xFF60efff),
-                ],
+                colors: AppColors.gradientAppBar,
               ),
             ),
           ),
         ),
         Positioned(
-            top: 16,
+            top: 16.h,
             child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              height: 56,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16, vertical: 8).r,
+              height: 56.h,
               width: MediaQuery.of(context).size.width,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Icon(
+                children: [
+                  const Icon(
                     Icons.arrow_back_ios,
-                    color: Colors.white,
+                    color: AppColors.backgroundColor,
                   ),
-                  Text(
-                    "Edit profile",
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600),
-                  ),
-                  Icon(
+                  TextsComponents.bigText('My profile',
+                      fontSize: 20, color: AppColors.backgroundColor),
+                  const Icon(
                     Icons.share,
-                    color: Colors.white,
+                    color: AppColors.backgroundColor,
                   ),
                 ],
               ),
             )),
       ],
     );
-
-    // SizedBox(
-    //   height: 120,
-    //   child: ClipPath(
-    //     clipper: Customshape(),
-    //     child: Stack(
-    //       alignment: AlignmentDirectional.center,
-    //       children: [
-    //         Container(
-    //           width: MediaQuery.of(context).size.width,
-    //           decoration: BoxDecoration(
-    //             //color: Colors.red,
-    //             boxShadow: [
-    //               BoxShadow(
-    //                 color: Colors.grey.withOpacity(0.5),
-    //                 spreadRadius: 5,
-    //                 blurRadius: 7,
-    //                 offset: const Offset(0, 3), // changes position of shadow
-    //               ),
-    //             ],
-    //             gradient: const LinearGradient(
-    //               begin: Alignment.center,
-    //               end: Alignment.bottomCenter,
-    //               colors: [
-    //                 Color(0xFF0061ff),
-    //                 Color(0xFF60efff),
-    //               ],
-    //             ),
-    //           ),
-    //         ),
-    //         Positioned(
-    //             top: 16,
-    //             child: Container(
-    //               padding:
-    //                   const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-    //               height: 56,
-    //               width: MediaQuery.of(context).size.width,
-    //               child: Row(
-    //                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-    //                 children: const [
-    //                   Icon(
-    //                     Icons.arrow_back_ios,
-    //                     color: Colors.white,
-    //                   ),
-    //                   Text(
-    //                     "Edit profile",
-    //                     style: TextStyle(
-    //                         fontSize: 20,
-    //                         color: Colors.white,
-    //                         fontWeight: FontWeight.w600),
-    //                   ),
-    //                   Icon(
-    //                     Icons.share,
-    //                     color: Colors.white,
-    //                   ),
-    //                 ],
-    //               ),
-    //             )),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
